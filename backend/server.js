3bbd7a3083f08d603776e5bce1b8e69bd568b570
shreyansh <shreyansh.hurkat70@gmail.com>
2024-05-08 21:06:57 +0530

    Deployment commits

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,10 @@ import messageRoutes from './Routers/message.routes.js';
 import userRoutes from './Routers/user.routes.js';
 import cookieParser from 'cookie-parser';
 import {app, server} from './socket/socket.js';
+import path from 'path'
 
 const port = process.env.PORT || 5000
-
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -22,6 +23,13 @@ app.use(cookieParser());
 app.use('/api/auth' , authRoutes);
 app.use('/api/messages' , messageRoutes);
 app.use('/api/user' , userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get('*' , (req, res) => {
+    res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"));
+})
+
 
 server.listen(port, () => {
     connectToMongoDB();
